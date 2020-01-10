8d6c673b0d06d50ecb304090038117c05ac008c0
muzammal781 <muzammalikram781@gmail.com>
2020-01-10 15:58:14 +0500

    sdsd

--- pull.php
+++ pull.php
@@ -1,8 +1,12 @@
 <?php
 if (isset($_GET['challenge'])) {
 	echo $_GET['challenge'];
+}else if(isset($_GET)){
+	echo "get part";
+}else if(isset($_POST)){
+	echo "post part";
 }else{
-	echo "else part";
+	echo "else";
 }
 
 
